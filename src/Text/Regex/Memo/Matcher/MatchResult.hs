c3e7987eb1536e530b623389cffc24adac21e68b
0xd34df00d <0xd34df00d@gmail.com>
2024-08-21 11:44:26 -0500

    Derive NFData for MatchResult

--- src/Text/Regex/Memo/Matcher/MatchResult.hs
+++ src/Text/Regex/Memo/Matcher/MatchResult.hs
@@ -1,11 +1,18 @@
+{-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE Strict #-}
 
 module Text.Regex.Memo.Matcher.MatchResult(MatchResult(..)) where
 
 import Control.Applicative
+import GHC.Generics
+import Control.DeepSeq
 
-data MatchResult a = SuccessAt a | Failure deriving (Eq, Ord, Show, Functor)
+data MatchResult a = SuccessAt a | Failure
+  deriving stock (Eq, Ord, Show, Functor, Generic)
+  deriving anyclass (NFData)
 
 instance Applicative MatchResult where
   SuccessAt f <*> SuccessAt v = SuccessAt $ f v
