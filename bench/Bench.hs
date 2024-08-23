737eab208b5f2abd06494ff1990bb4aba561c79f
0xd34df00d <0xd34df00d@gmail.com>
2024-08-23 12:17:56 -0500

    Add DoS benchmarks

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -13,6 +13,7 @@ import Text.Regex.Memo.Matcher.Memoizing qualified as M
 main :: IO ()
 main = do
   Right nfa <- pure $ convert <$> parseRx "(aa|ab)*z"
+  Right dos <- pure $ convert <$> parseRx "(a|a)*z"
   defaultMain
     [ bgroup "naive matcher" $
       [ bench (show len <> " pat " <> show pat <> " end " <> show end ) $ nf (N.match nfa) $ BS.concat (replicate len pat) <> end
@@ -26,6 +27,15 @@ main = do
       , pat <- ["aa", "ab"]
       , end <- ["", "z"]
       ]
+    , bgroup "dos (naive)" $
+      [ bench (show len) $ nf (N.match dos) $ BS.replicate len 'a' <> "w"
+      | len <- dosLens
+      ]
+    , bgroup "dos (memo)" $
+      [ bench (show len) $ nf (M.match dos) $ BS.replicate len 'a' <> "w"
+      | len <- dosLens
+      ]
     ]
   where
   lens = [100_000]
+  dosLens = [10, 12 .. 20]
